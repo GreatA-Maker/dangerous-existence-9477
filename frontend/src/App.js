7546d63d567ddd883bf31e69770fe58fda5e63e7
GreatA-Maker <75976912+GreatA-Maker@users.noreply.github.com>
2022-12-16 08:14:59 +0530

    Empty Cart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
+import EmptyCart from "./Components/Cart/EmptyCart";
 import ProductSection from "./Components/ProductSection";
 import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
 	return (
 		<div className="App">
-			<AllRoutes />
+			{/* <AllRoutes /> */}
+			<EmptyCart />
 		</div>
 	);
 }
