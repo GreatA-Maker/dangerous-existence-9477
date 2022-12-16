703c790a333b6fd3e5e02260133deab5b25892b2
sks-7 <sachinsinghofficial7@gmail.com>
2022-12-16 22:04:16 +0530

    added day_4 task

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,10 @@
 import "./App.css";
 import ProductSection from "./Components/ProductSection";
-import Address from "./Pages/AddressPage/Address";
-import Homepage from "./Pages/HomePage/Homepage";
 import AllRoutes from "./Routes/AllRoutes";
-import Footer from "./Components/Footer/Footer"
-import Navbar from "./Components/Navbar/Navbar"
-
 function App() {
 return (
   <div className="App">
-    {/* <AllRoutes /> */}
-    <Navbar />
-    <Homepage />
-    {/* <Address/> */}
-    <Footer />
+    <AllRoutes />
   </div>
 );
 
