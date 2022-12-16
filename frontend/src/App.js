74a80ef3fc7efff6c9b4feaa04699edee93b3b08
GreatA-Maker <75976912+GreatA-Maker@users.noreply.github.com>
2022-12-16 05:50:03 +0530

    Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,13 @@
-import './App.css';
-import ProductSection from './Components/ProductSection';
-import AllRoutes from './Routes/AllRoutes';
-
+import "./App.css";
+import ProductSection from "./Components/ProductSection";
+import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
-  return (
-    <div className="App">
-
-      <AllRoutes />
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<AllRoutes />
+		</div>
+	);
 }
 
 export default App;
