4fd8d5c83db70e4cf312c7041c57d64629853ffa
GreatA-Maker <75976912+GreatA-Maker@users.noreply.github.com>
2022-12-16 09:56:26 +0530

    Cart Empty And Buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import "./App.css";
-import EmptyCart from "./Components/Cart/EmptyCart";
 import ProductSection from "./Components/ProductSection";
 import AllRoutes from "./Routes/AllRoutes";
 
 function App() {
 	return (
 		<div className="App">
-			{/* <AllRoutes /> */}
-			<EmptyCart />
+			<AllRoutes />
 		</div>
 	);
 }
