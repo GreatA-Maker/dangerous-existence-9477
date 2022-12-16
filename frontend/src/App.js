b935cc5899f560e099569e67009929867ccbdb80
sks-7 <sachinsinghofficial7@gmail.com>
2022-12-16 10:06:34 +0530

    added address reponsive and backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import ProductSection from './Components/ProductSection';
-import Address from './Pages/AddressPage/Address';
 import AllRoutes from './Routes/AllRoutes';
 
 
 function App() {
   return (
     <div className="App">
-
-      {/* <AllRoutes /> */}
-      <Address/>
-
+      <AllRoutes />
     </div>
   );
 }
